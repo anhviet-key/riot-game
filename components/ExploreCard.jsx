f00e6590062ff11c885d843311ac66163bf16ee3
anhviet-key <anhviet.key@gmail.com>
2023-03-13 11:11:53 +0700

    #1 commit

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -19,7 +19,9 @@ const ExploreCard = ({
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
     className={`relative ${
-      active === id ? 'lg:flex-[3.5] flex:10' : 'lg:flex-[0.5] flex-[2]'
+      active === id
+        ? 'lg:flex-[3.5] flex:10'
+        : 'lg:flex-[0.5] flex-[2] min-h-[150px]'
     } flex items-center justify-center min-w-[170px] h-[400px] lg:h-[700px] transition-[flex] duration-[1s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
